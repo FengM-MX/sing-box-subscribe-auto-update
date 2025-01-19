a305d3c3ad52bf026c168c1b875568f61e9b5858
FengM-MX <33050478+FengM-MX@users.noreply.github.com>
2025-01-19 13:56:30 +0800

    初始化代码库

---
+++ dockerfile
@@ -0,0 +1,30 @@
+FROM python:3.11-slim
+# 设置用户
+ENV USER_ID=1000
+ENV GROUP_ID=1000
+
+# 确保用户权限并拉取代码
+RUN \
+    mkdir -m 777 /tmp/sing-box-subscribe /sing-box-subscribe && \
+    apt update && \
+    apt install -y git gosu && \
+    git clone https://github.com/Toperlock/sing-box-subscribe.git /tmp/sing-box-subscribe && \
+    sed 's/"exclude_protocol":"ssr"/"exclude_protocol":""/g' /tmp/sing-box-subscribe/providers.json && \
+    git config --global core.filemode false
+
+# 切换用户
+
+COPY ./start.sh /root/start.sh
+
+WORKDIR /sing-box-subscribe
+
+# 安装依赖
+RUN \
+    chmod +x /root/start.sh && \
+    cd /tmp/sing-box-subscribe && \
+    pip install --no-cache-dir --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt
+
+
+EXPOSE 5000
+ENTRYPOINT ["/root/start.sh"]
---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# 如果指定了 USER_ID，则创建用户
+if ! id -u $USER_ID > /dev/null 2>&1; then
+    groupadd -g $GROUP_ID subscribe
+    useradd -u $USER_ID -g subscribe -m subscribe
+fi
+
+echo "copy the program from /tmp/sing-box-subscribe to /sing-box-subscribe"
+# 文件不存在，则移动程序文件
+if [ ! -d "/sing-box-subscribe" ];then
+    echo "create dest dir..."
+    mkdir /sing-box-subscribe
+fi
+if [ ! -f "/sing-box-subscribe/api/app.py" ];then
+   echo "copy files to dest dir..."
+   #find /tmp/sing-box-subscribe -mindepth 1 -maxdepth 1 -exec cp -rfv {} /sing-box-subscribe/ \;
+   shopt -s dotglob
+   cp -rfv /tmp/sing-box-subscribe /
+   #cp -rf /tmp/sing-box-subscribe/.git /sing-box-subscribe/
+   #cp -rf /tmp/sing-box-subscribe/.github /sing-box-subscribe/
+   #cp -rf /tmp/sing-box-subscribe/.gitignore /sing-box-subscribe/
+fi
+echo "grant permissions..."
+chown -R $USER_ID:$GROUP_ID /sing-box-subscribe
+chmod -R 755 /sing-box-subscribe
+# 拉取最新代码
+echo "start pulling the latest code"
+gosu $USER_ID bash -c 'cd /sing-box-subscribe && \
+    git stash save && \
+    git pull --rebase && \
+    git stash pop && \
+    pip install --no-cache-dir --upgrade pip && \
+    pip install --no-cache-dir -r requirements.txt'
+
+# 启动项目
+echo "Starting application..."
+gosu $USER_ID bash -c "python /sing-box-subscribe/api/app.py"
